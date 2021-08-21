460d08a335c1a3287b678eb2bcd2d5e558b1a2ce
Vireak Ouk <vireak.ouk@gmail.com>
2021-08-21 12:26:14 +0700

    test underlay

---
+++ clear_underlay.py
@@ -0,0 +1,25 @@
+from nornir import InitNornir
+from nornir.core.task import Task, Result
+from nornir_utils.plugins.functions import print_result
+from nornir_jinja2.plugins.tasks import template_file
+from nornir_scrapli.tasks import send_command, send_config
+
+def deploy_int(task: Task) -> Result:
+    r = task.run(task=template_file, 
+                template="clear_underlay.j2",
+                path="./templates")
+    task.host["config"] = r.result
+
+    task.run(task=send_config, 
+            name="Clearing underlay!", 
+            dry_run=False,
+            config=task.host["config"])
+    
+    task.run(task=send_command,
+            name="Show new config",
+            command="show run")
+
+if __name__ == "__main__":
+    nr = InitNornir(config_file="config.yml")
+    r = nr.run(task=deploy_int)
+    print_result(r)
