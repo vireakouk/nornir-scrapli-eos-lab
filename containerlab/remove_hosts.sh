faff0631f45967085aadc6d2b40e2ad53a29e2cc
Vireak Ouk <vireak.ouk@gmail.com>
2021-08-30 21:55:44 +0700

    added evpn overlay

--- containerlab/remove_hosts.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/bash
-ssh-keygen -f "/home/vireak/.ssh/known_hosts" -R "172.100.1.11"
-ssh-keygen -f "/home/vireak/.ssh/known_hosts" -R "172.100.1.12"
-ssh-keygen -f "/home/vireak/.ssh/known_hosts" -R "172.100.1.13"
-ssh-keygen -f "/home/vireak/.ssh/known_hosts" -R "172.100.1.14"
